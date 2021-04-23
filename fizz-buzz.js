0fa61dd5266347e489e6fc734a46136bbc2b2851
Martin Lienhart <martin.lienhart@outlook.com>
2021-04-23 21:48:19 +0200

    fizz-buzz

---
+++ fizz-buzz.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+    let a = [];
+
+    for (i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) { a.push('FizzBuzz'); }
+        else if (i % 3 === 0) { a.push('Fizz'); }
+        else if (i % 5 === 0) { a.push('Buzz'); }
+        else { a.push(i.toString()); }
+    };
+
+    return a;
+};
