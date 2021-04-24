ace17ee651e69cce98d807bd59416a1440576fea
Martin Lienhart <martin.lienhart@outlook.com>
2021-04-24 23:34:51 +0200

    third-maximum-number

---
+++ third-maximum-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+    let a = [...new Set(nums)];
+    let b = a.sort((a, b) => a > b ? -1 : 1);
+
+    return b[2] ?? Math.max(...nums);
+};
