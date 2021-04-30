5201b2060dedf0185c8c159da49138ca4927559e
Martin Lienhart <martin.lienhart@outlook.com>
2021-04-30 23:23:02 +0200

    add-digits

--- add-digits.js
+++ add-digits.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var addDigits = function (num) {
-    if (num < 10) { return num; }
+    if (num === 0) { return 0 }
     else if (num % 9 === 0) { return 9; }
     else { return num % 9; }
 };
