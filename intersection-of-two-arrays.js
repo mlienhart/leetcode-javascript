d5b71e5aad716b60328faf4c8d80b40534ea6d59
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-01 11:22:23 +0200

    intersection-of-two-arrays

---
+++ intersection-of-two-arrays.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+    let a = new Set(nums1);
+    let b = new Set();
+
+    for (i = 0; i < nums2.length; i++) {
+        if (a.has(nums2[i])) {
+            b.add(nums2[i]);
+        }
+    }
+
+    return [...b];
+};
