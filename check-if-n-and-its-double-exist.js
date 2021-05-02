181f31df6799db4cc6f2edc0cc2cbc3358afb378
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-02 14:25:26 +0200

    check-if-n-and-its-double-exist

---
+++ check-if-n-and-its-double-exist.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+    let a = new Set();
+
+    for (i = 0; i < arr.length; i++) {
+        if (a.has(arr[i] * 2) || a.has(arr[i] / 2)) {
+            return true;
+        }
+        a.add(arr[i]);
+    }
+
+    return false;
+};
