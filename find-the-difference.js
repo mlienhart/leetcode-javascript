c01641b8e20e4c2ed0134c7ca622448e84fe4eac
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-03 17:18:28 +0200

    find-the-difference

---
+++ find-the-difference.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+    let a = s.split('');
+    let b = t.split('');
+    let c = [];
+    let d = [];
+
+    for (i = 0; i < s.length; i++) {
+        c.push(a[i].charCodeAt());
+    }
+
+    for (i = 0; i < t.length; i++) {
+        d.push(b[i].charCodeAt());
+    }
+
+    return String.fromCharCode(d.reduce((a, b) => a + b, 0) - c.reduce((a, b) => a + b, 0));
+};
