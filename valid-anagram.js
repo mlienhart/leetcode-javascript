8ce0ec6bc94b36a4a8cb1ee18653153c0a0c3a48
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-07 22:00:56 +0200

    valid-anagram

---
+++ valid-anagram.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    let a = s.split('').sort((a, b) => a > b ? 1 : -1).join();
+    let b = t.split('').sort((a, b) => a > b ? 1 : -1).join();
+
+    return a === b ? true : false;
+};
