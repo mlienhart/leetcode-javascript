828ca08f9001f50bde704ab4825aa6a8151e220e
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-09 23:31:02 +0200

    find-numbers-with-even-number-of-digits

---
+++ find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function (nums) {
+    let a = 0;
+
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i].toString().length % 2 === 0) {
+            a++;
+        }
+    }
+
+    return a;
+};
