0a71ecc205667b08f8ff1bf67dbf3c198aa03f44
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-13 21:55:51 +0200

    group-anagrams

---
+++ group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+    let map = new Map();
+
+    for (let x of strs) {
+        let a = x.split("").sort((a, b) => a < b ? -1 : 1).join("");
+        if (!map.get(a)) {
+            map.set(a, []);
+        }
+        map.get(a).push(x);
+    }
+
+    return [...map.values()];
+};
