947d88b023211fbad14ab0f5a3951ce84297b5b4
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-15 11:48:02 +0200

    flipping-an-image

---
+++ flipping-an-image.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (image) {
+    let a = (b) => {
+        for (i = 0; i < b.length; i++) {
+            if (b[i] === 0) {
+                b[i]++;
+            }
+            else {
+                b[i]--;
+            };
+        }
+        return b;
+    };
+
+    let b = image.map(x => x.reverse());
+
+    return b.map(x => a(x));
+};
