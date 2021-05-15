2d7389ee5eff64332e9fb0312cc9658ec8fa8d5c
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-15 23:40:23 +0200

    find-lucky-integer-in-an-array

---
+++ find-lucky-integer-in-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findLucky = function (arr) {
+    let map = new Map;
+    let a = [];
+
+    for (i = 0; i < arr.length; i++) {
+        map.set(arr[i], (map.get(arr[i]) || 0) + 1);
+    }
+
+    for ([key, value] of map.entries()) {
+        if (key === value) {
+            a.push(value);
+        }
+    }
+
+    return (a.length > 0) ? Math.max(...a) : -1;
+};
