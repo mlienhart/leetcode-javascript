9b68ee03ee4cec545024244f53876df8f4387e88
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-16 23:01:16 +0200

    merge-strings-alternately

---
+++ merge-strings-alternately.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function (word1, word2) {
+    let a = word1.split("");
+    let b = word2.split("");
+    let arr = [];
+
+    for (i = 0; i <= 100; i++) {
+        arr.push(a[i]);
+        arr.push(b[i]);
+    }
+
+    return arr.join("");
+};
