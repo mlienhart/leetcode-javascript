35e9d2c4bd361fc4a440b0296b1a88dd6d4b898b
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-16 23:26:14 +0200

    generate-a-string-with-characters-that-have-odd-counts

---
+++ generate-a-string-with-characters-that-have-odd-counts.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var generateTheString = function (n) {
+    let a = "a";
+    let b = "b";
+
+    if (n % 2 !== 0) {
+        return a.repeat(n);
+    }
+
+    return a.repeat(n - 1) + b;
+};
