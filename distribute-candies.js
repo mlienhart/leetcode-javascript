c4d2755f9dbba883afef06a3b82106c8e959ad84
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-17 23:27:53 +0200

    distribute-candies

---
+++ distribute-candies.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function (candyType) {
+    let a = candyType.length / 2;
+    let b = new Set(candyType).size;
+
+    return a > b ? b : a;
+};
