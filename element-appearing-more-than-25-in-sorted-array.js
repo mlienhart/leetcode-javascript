86fcf990d55871efecda5803f939503fc1b9ff48
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-23 20:36:00 +0200

    element-appearing-more-than-25-in-sorted-array

---
+++ element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var findSpecialInteger = function (arr) {
+    let map = new Map();
+
+    for (i = 0; i < arr.length; i++) {
+        map.set(arr[i], (map.get(arr[i]) || 0) + 1);
+    }
+
+    for ([key, value] of map.entries()) {
+        if (value > (arr.length / 4)) {
+            return key;
+        };
+    }
+};
