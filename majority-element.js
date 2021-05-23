531fd013ccce62c450a32b48edcf361bb482705e
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-23 20:52:09 +0200

    majority-element

---
+++ majority-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let map = new Map();
+    let a = [];
+
+    for (i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+    }
+
+    for ([key, value] of map.entries()) {
+        a.push({ key: key, value: value });
+    }
+
+    return a.sort((a, b) => a.value > b.value ? -1 : 1)[0].key;
+};
