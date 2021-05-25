961c6cbc06ae3459baca8b84a5155158a45cbce8
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-25 23:16:34 +0200

    sort-characters-by-frequency

---
+++ sort-characters-by-frequency.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+    let a = s.split("");
+    let map = new Map();
+    let arr = [];
+
+    for (i = 0; i < a.length; i++) {
+        map.set(a[i], (map.get(a[i]) || 0) + 1);
+    }
+
+    for ([key, value] of map.entries()) {
+        arr.push({ key: key, value: value });
+    }
+
+    let b = arr.sort((a, b) => a.value > b.value ? -1 : 1);
+
+    return b.map(x => x.key.repeat(x.value)).join("");
+};
