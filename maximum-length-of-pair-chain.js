15ff51623e4dcacbeeaa94afbd199a13bf310ac6
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-26 23:57:39 +0200

    maximum-length-of-pair-chain

---
+++ maximum-length-of-pair-chain.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} pairs
+ * @return {number}
+ */
+var findLongestChain = function (pairs) {
+    pairs.sort((a, b) => a[1] < b[1] ? -1 : 1);
+
+    let a = 0;
+    let b = -1000;
+
+    for ([x, y] of pairs) {
+        if (x > b) {
+            b = y;
+            a++;
+        }
+    }
+
+    return a;
+};
