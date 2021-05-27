7c0c54bb048478e8e7ceffbef1f850db1ce335fa
Martin Lienhart <martin.lienhart@outlook.com>
2021-05-27 18:25:03 +0200

    max-consecutive-ones

---
+++ max-consecutive-ones.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function (nums) {
+    let a = nums.join("").split("0").sort((a, b) => a > b ? -1 : 1);
+
+    return a[0].length;
+};
