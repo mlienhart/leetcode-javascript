a74ab7b9fd156ba68df4338c0827f1ebda473229
Martin Lienhart <martin.lienhart@outlook.com>
2021-06-05 23:43:57 +0200

    thousand-separator

---
+++ thousand-separator.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var thousandSeparator = function (n) {
+    let a = `${n}`.split("").reverse().join("");
+    let b = [];
+
+    for (i = 0; i < a.length; i++) {
+        if (i % 3 === 0 && i > 0) {
+            b.push(".");
+        }
+        b.push(a[i]);
+    }
+
+    return b.reverse().join("");
+};
