bfea5cea7f3b24f36baeb652ba1d1b007645d2f3
Martin Lienhart <martin.lienhart@outlook.com>
2021-06-14 23:04:12 +0200

    latest-time-by-replacing-hidden-digits

---
+++ latest-time-by-replacing-hidden-digits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} time
+ * @return {string}
+ */
+var maximumTime = function (time) {
+    let a = time.split("");
+
+    if (a[0] === "?") { a[0] = a[1] > 3 ? "1" : "2"; }
+    if (a[1] === "?") { a[1] = a[0] > 1 ? "3" : "9"; }
+    if (a[3] === "?") { a[3] = "5"; }
+    if (a[4] === "?") { a[4] = "9"; }
+
+    return a.join("");
+};
