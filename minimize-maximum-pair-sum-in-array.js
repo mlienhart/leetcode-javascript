7a26d09e938586909e07d322a3eef1f354750803
Martin Lienhart <martin.lienhart@outlook.com>
2021-06-18 22:41:23 +0200

    minimize-maximum-pair-sum-in-array

--- minimize-maximum-pair-sum-in-array.js
+++ minimize-maximum-pair-sum-in-array.js
@@ -4,11 +4,11 @@
  */
 var minPairSum = function (nums) {
     nums.sort((a, b) => a < b ? -1 : 1);
-    let b = 0;
+    let a = 0;
 
     for (i = 0; i < nums.length / 2; i++) {
-        b = Math.max(b, nums[i] + nums[nums.length - i - 1]);
+        a = Math.max(a, nums[i] + nums[nums.length - i - 1]);
     }
 
-    return b;
+    return a;
 };
