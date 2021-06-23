2329e1d6cfd99d37f546f3566baa470ac380fb78
Martin Lienhart <martin.lienhart@outlook.com>
2021-06-23 22:21:17 +0200

    minimum-distance-to-the-target-element

---
+++ minimum-distance-to-the-target-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+var getMinDistance = function (nums, target, start) {
+    let a = 1000;
+
+    for (i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            a = Math.min(a, Math.abs(i - start));
+        }
+    }
+
+    return a;
+};
