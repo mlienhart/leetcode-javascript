6027f7c0d6480d11b997ecd00ccf1de96f8e8f99
Martin Lienhart <martin.lienhart@outlook.com>
2021-06-29 17:05:05 +0200

    break-a-palindrome

---
+++ break-a-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+var breakPalindrome = function (palindrome) {
+    let a = palindrome.split("");
+
+    for (i = 0; i < Math.floor(a.length / 2); i++) {
+        if (a[i] !== "a") {
+            a[i] = "a";
+            return a.join("");
+        }
+    }
+
+    if (a.length === 1) {
+        return "";
+    }
+    else {
+        a[a.length - 1] = "b";
+        return a.join("");
+    }
+};
