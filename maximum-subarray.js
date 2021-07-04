2f112e7ee3c0570f9adad01cbeddebee6ed1df02
Martin Lienhart <martin.lienhart@outlook.com>
2021-07-04 23:48:16 +0200

    maximum-subarray

---
+++ maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let a = nums[0];
+    let b = nums[0];
+
+    for (i = 1; i < nums.length; i++) {
+        a = Math.max(nums[i], a + nums[i]);
+        b = Math.max(b, a);
+    }
+
+    return b;
+};
