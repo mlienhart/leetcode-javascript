6dfb41484bdad2b242a814d7ab067752dc253b7e
Martin Lienhart <martin.lienhart@outlook.com>
2021-07-21 00:24:12 +0200

    day-of-the-week

---
+++ day-of-the-week.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+  let a = new Date(year, month - 1, day).toString().substring(0, 3);
+  let b = {
+    Sun: "Sunday",
+    Mon: "Monday",
+    Tue: "Tuesday",
+    Wed: "Wednesday",
+    Thu: "Thursday",
+    Fri: "Friday",
+    Sat: "Saturday",
+  };
+
+  return b[a];
+};
