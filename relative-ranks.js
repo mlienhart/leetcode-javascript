d6f9bec9578d4872fc54b4dd672ee1c3a231da0b
Martin Lienhart <martin.lienhart@outlook.com>
2021-07-28 21:41:08 +0200

    relative-ranks

---
+++ relative-ranks.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function (score) {
+  let a = [];
+  let b = score.map((x) => x).sort((a, b) => (a > b ? -1 : 1));
+
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] === b[0]) {
+      a.push({ c: "Gold Medal" });
+    } else if (score[i] === b[1]) {
+      a.push({ c: "Silver Medal" });
+    } else if (score[i] === b[2]) {
+      a.push({ c: "Bronze Medal" });
+    } else {
+      a.push({ c: `${b.indexOf(score[i]) + 1}` });
+    }
+  }
+
+  return a.map((x) => x.c);
+};
