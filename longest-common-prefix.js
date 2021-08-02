48cf3cf4e560130b943a88f77ff8c0d6dbf4f72d
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-02 13:00:49 +0200

    longest-common-prefix

---
+++ longest-common-prefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let a = [];
+  let b = strs.map((x) => x).sort((a, b) => (a > b ? -1 : 1));
+
+  for (let i = 0; i < b[0].length; i++) {
+    if (strs.every((x) => x[i] === strs[0][i]) && i === a.length) {
+      a.push(strs[0][i]);
+    }
+  }
+
+  return a.length ? a.join("") : "";
+};
