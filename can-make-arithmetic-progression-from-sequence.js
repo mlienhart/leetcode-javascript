dfe1bbadeefdd2a3fd9d5b44e0236333b5f82f4d
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-04 16:20:46 +0200

    can-make-arithmetic-progression-from-sequence

---
+++ can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function (arr) {
+  arr.sort((a, b) => (a < b ? -1 : 1));
+
+  let a = arr[1] - arr[0];
+  let b = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] === a) {
+      b++;
+    }
+  }
+
+  return b === arr.length - 1;
+};
