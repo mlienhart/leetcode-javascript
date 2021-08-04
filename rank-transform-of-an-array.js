5e0c8195f53cac94412d65b052d172fde37dadab
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-04 22:01:44 +0200

    rank-transform-of-an-array

---
+++ rank-transform-of-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function (arr) {
+  let a = [...new Set(arr.map((x) => x).sort((a, b) => a - b))];
+  let b = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    b.push(a.indexOf(arr[i]) + 1);
+  }
+
+  return b;
+};
