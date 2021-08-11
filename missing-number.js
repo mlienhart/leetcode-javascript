7d86db1cc9e6d96fc91fb83faaaa9fbdd636f030
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-11 08:26:12 +0200

    missing-number

---
+++ missing-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  nums.sort((a, b) => (a < b ? -1 : 1));
+
+  if (nums[0] === 1) {
+    return 0;
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] + 1 !== nums[i + 1]) {
+      return nums[i] + 1;
+    }
+  }
+};
