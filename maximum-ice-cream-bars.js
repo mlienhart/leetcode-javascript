ba6bf8e72b1e3264605eff961f8434140861369a
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-12 09:39:52 +0200

    maximum-ice-cream-bars

---
+++ maximum-ice-cream-bars.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} costs
+ * @param {number} coins
+ * @return {number}
+ */
+var maxIceCream = function (costs, coins) {
+  costs.sort((a, b) => (a < b ? -1 : 1));
+
+  let a = 0;
+
+  for (let i = 0; i < costs.length; i++) {
+    if (costs[i] <= coins) {
+      a++;
+      coins = coins - costs[i];
+    }
+  }
+
+  return a;
+};
