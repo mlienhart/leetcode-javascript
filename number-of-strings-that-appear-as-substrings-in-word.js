b30a7aa4a69361ae977de4b3bd0ab0f48ca03240
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-15 15:29:36 +0200

    number-of-strings-that-appear-as-substrings-in-word

---
+++ number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function (patterns, word) {
+  let a = 0;
+
+  for (let i = 0; i < patterns.length; i++) {
+    if (word.includes(patterns[i])) {
+      a++;
+    }
+  }
+
+  return a;
+};
