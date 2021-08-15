6fb2efcab86954a11d4b0cdf25f99ff9052ca340
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-15 15:03:55 +0200

    transpose-matrix

---
+++ transpose-matrix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function (matrix) {
+  let a = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      a[j] = a[j] || [];
+      a[j][i] = matrix[i][j];
+    }
+  }
+
+  return a;
+};
