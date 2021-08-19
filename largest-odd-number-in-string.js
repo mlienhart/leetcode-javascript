7a2ed47c02d532bb00f63ad1196c6da635e1ae72
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-20 00:07:04 +0200

    largest-odd-number-in-string

---
+++ largest-odd-number-in-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var largestOddNumber = function (num) {
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (num[i] % 2 !== 0) {
+      return num;
+    }
+    num = num.slice(0, -1);
+  }
+
+  return num;
+};
