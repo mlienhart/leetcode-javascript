3e2f9b60827f1e8ca489a78964f97a2afafa748d
Martin Lienhart <martin.lienhart@outlook.com>
2021-08-29 11:56:56 +0200

    add-digits

--- add-digits.js
+++ add-digits.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
 var addDigits = function (num) {
-    if (num === 0) { return 0; }
-    else if (num % 9 === 0) { return 9; }
-    else { return num % 9; }
+  if (num === 0) {
+    return 0;
+  } else if (num % 9 === 0) {
+    return 9;
+  } else {
+    return num % 9;
+  }
 };
