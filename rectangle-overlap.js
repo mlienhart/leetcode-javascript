fc574f827a4ddece947397a899b1766305767c27
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-01 21:14:53 +0200

    refactoring

--- rectangle-overlap.js
+++ rectangle-overlap.js
@@ -4,12 +4,8 @@
  * @return {boolean}
  */
 var isRectangleOverlap = function (rec1, rec2) {
-  if (
+  return (
     Math.min(rec1[2], rec2[2]) - Math.max(rec1[0], rec2[0]) > 0 &&
     Math.min(rec1[3], rec2[3]) - Math.max(rec1[1], rec2[1]) > 0
-  ) {
-    return true;
-  }
-
-  return false;
+  );
 };
