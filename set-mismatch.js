8f2bdc899ef0f218ca714d3942c7fa45b672c25e
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-01 22:03:57 +0200

    set-mismatch

---
+++ set-mismatch.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+  nums.sort((a, b) => (a < b ? -1 : 1));
+  let a = [];
+  let b = [...new Set([...nums])];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      a.push(nums[i]);
+    }
+  }
+
+  if (b[0] !== 1) {
+    a.push(1);
+    return a;
+  }
+
+  for (let i = 0; i < b.length; i++) {
+    if (b[i] + 1 !== b[i + 1]) {
+      a.push(b[i] + 1);
+      return a;
+    }
+  }
+
+  return a;
+};
