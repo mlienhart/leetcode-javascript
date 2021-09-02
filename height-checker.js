ad20b9df78f9dbb2c4794a089de733fc4a50e253
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-02 20:54:48 +0200

    formatting

--- height-checker.js
+++ height-checker.js
@@ -4,8 +4,9 @@
  */
 var heightChecker = function (heights) {
   let a = [...heights];
-  heights.sort((a, b) => (a < b ? -1 : 1));
   let b = 0;
+
+  heights.sort((a, b) => (a < b ? -1 : 1));
 
   for (let i = 0; i < heights.length; i++) {
     if (a[i] !== heights[i]) {
