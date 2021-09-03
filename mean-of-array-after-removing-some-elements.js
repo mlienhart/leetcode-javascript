4a2f876b193c8d9edcf69fc0818b4b1d68e543c4
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-03 22:25:09 +0200

    mean-of-array-after-removing-some-elements

---
+++ mean-of-array-after-removing-some-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var trimMean = function (arr) {
+  let a = arr.length * 0.05;
+
+  arr.sort((a, b) => (a < b ? -1 : 1));
+
+  for (let i = 0; i < a; i++) {
+    arr.shift();
+    arr.pop();
+  }
+
+  let b = arr.reduce((a, b) => a + b, 0) / arr.length;
+
+  return parseFloat(b.toFixed(5));
+};
