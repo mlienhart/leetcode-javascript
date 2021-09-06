f605c72ffec08dfb42fa812430d35c73fe54f37d
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-06 22:27:31 +0200

    refactoring

--- number-of-good-pairs.js
+++ number-of-good-pairs.js
@@ -5,8 +5,8 @@
 var numIdenticalPairs = function (nums) {
   let a = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
+  for (i = 0; i < nums.length - 1; i++) {
+    for (j = i + 1; j < nums.length; j++) {
       if (nums[i] === nums[j]) {
         a++;
       }
