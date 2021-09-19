d2240002cc2dd1abb6d0007373091bfef034177e
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-19 11:29:09 +0200

    refactoring

--- maximum-product-difference-between-two-pairs.js
+++ maximum-product-difference-between-two-pairs.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var maxProductDifference = function (nums) {
-  let a = nums.map((x) => x).sort((a, b) => (a < b ? -1 : 1));
-  let b = nums.map((x) => x).sort((a, b) => (a > b ? -1 : 1));
+  let a = [...nums].sort((a, b) => (a > b ? 1 : -1));
+  let b = [...nums].sort((a, b) => (a < b ? 1 : -1));
 
   return b[0] * b[1] - a[0] * a[1];
 };
