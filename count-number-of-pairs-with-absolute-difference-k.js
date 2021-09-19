e762b593699b28dd4e20b70104ecc682faf3f158
Martin Lienhart <martin.lienhart@outlook.com>
2021-09-19 22:13:45 +0200

    count-number-of-pairs-with-absolute-difference-k

---
+++ count-number-of-pairs-with-absolute-difference-k.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var countKDifference = function (nums, k) {
+  let a = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i < j && Math.abs(nums[i] - nums[j]) === k) {
+        a++;
+      }
+    }
+  }
+
+  return a;
+};
