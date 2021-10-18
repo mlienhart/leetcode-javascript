b1244d50fce54835215d60dd3a20a4d0d61de16d
Martin Lienhart <martin.lienhart@outlook.com>
2021-10-18 23:20:15 +0200

    remove-colored-pieces-if-both-neighbors-are-the-same-color

---
+++ remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} colors
+ * @return {boolean}
+ */
+var winnerOfGame = function (colors) {
+  let a = 0;
+  let b = 0;
+
+  for (let i = 1; i < colors.length - 1; i++) {
+    if (colors[i - 1] === colors[i] && colors[i + 1] === colors[i]) {
+      colors[i] === "A" ? a++ : b++;
+    }
+  }
+
+  return a > b;
+};
