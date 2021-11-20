8bfef3d50ce9010e2925a40ab2ee4643a04978f4
Martin Lienhart <martin.lienhart@outlook.com>
2021-11-20 13:13:12 +0100

    convert-integer-to-the-sum-of-two-no-zero-integers

--- convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -4,7 +4,7 @@
  */
 var getNoZeroIntegers = function (n) {
   for (let i = 0; i < n; i++) {
-    if (!i.toString().includes("0") && !(n - i).toString().includes("")) {
+    if (!i.toString().includes("0") && !(n - i).toString().includes("0")) {
       return [i, n - i];
     }
   }
