2d1c337e28e46ccc41af61f1ae67d90140eafc89
Martin Lienhart <martin.lienhart@outlook.com>
2021-11-21 16:27:46 +0100

    two-furthest-houses-with-different-colors

--- two-furthest-houses-with-different-colors.js
+++ two-furthest-houses-with-different-colors.js
@@ -5,8 +5,8 @@
 var maxDistance = function (colors) {
   let a = 0;
 
-  for (let i = 0; i < colors.length; i++) {
-    for (let j = 0; j < colors.length; j++) {
+  for (let i = 0; i < colors.length - 1; i++) {
+    for (let j = i + 1; j < colors.length; j++) {
       if (colors[i] !== colors[j]) {
         a = Math.max(a, Math.abs(i - j));
       }
