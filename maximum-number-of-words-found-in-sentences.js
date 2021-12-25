d8484379d3b8f75601a2c6715f8babe754dbb1cd
Martin Lienhart <martin.lienhart@outlook.com>
2021-12-25 21:41:07 +0100

    maximum-number-of-words-found-in-sentences

---
+++ maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function (sentences) {
+  let a = [];
+
+  for (let i = 0; i < sentences.length; i++) {
+    a.push(sentences[i].split(" ").length);
+  }
+
+  return Math.max(...a);
+};
