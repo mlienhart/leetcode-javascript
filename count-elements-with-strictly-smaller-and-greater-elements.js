44add5fc5969db5d5f2145828dd75d236aa2369b
Martin Lienhart <martin.lienhart@outlook.com>
2022-01-23 13:28:14 +0100

    count-elements-with-strictly-smaller-and-greater-elements

--- count-elements-with-strictly-smaller-and-greater-elements.js
+++ count-elements-with-strictly-smaller-and-greater-elements.js
@@ -6,9 +6,9 @@ var countElements = function (nums) {
   let a = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== Math.max(...nums) && nums[i] !== Math.min(...nums)) {
-      a++;
-    }
+    nums[i] !== Math.max(...nums) && nums[i] !== Math.min(...nums)
+      ? a++
+      : (a = a);
   }
 
   return a;
