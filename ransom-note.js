82c567a9237d49c3f589dd9a980e5e4eb39aaf6b
Martin Lienhart <martin.lienhart@outlook.com>
2022-03-14 19:38:05 +0100

    ransom-note

--- ransom-note.js
+++ ransom-note.js
@@ -4,16 +4,16 @@
  * @return {boolean}
  */
 var canConstruct = function (ransomNote, magazine) {
-  let a = ransomNote.split("");
-  let b = magazine.split("");
-  let c = 0;
+  a = ransomNote.split("");
+  b = magazine.split("");
+  c = 0;
 
   for (let i = 0; i < a.length; i++) {
     if (b.includes(a[i])) {
-      b.splice(b.indexOf(a[i]), 1);
       c++;
+      b.splice(b.indexOf(a[i]), 1);
     }
   }
 
-  return c === a.ransomNote;
+  return c === ransomNote.length;
 };
