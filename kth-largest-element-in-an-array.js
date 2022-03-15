46ccb132c841f6e6f3118c5d7c4a9d14d8e41ef6
Martin Lienhart <martin.lienhart@outlook.com>
2022-03-15 22:59:28 +0100

    kth-largest-element-in-an-array

--- kth-largest-element-in-an-array.js
+++ kth-largest-element-in-an-array.js
@@ -4,7 +4,5 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-  let a = [...nums].sort((a, b) => (a >= b ? -1 : 1));
-
-  return a[k - 1];
+  return nums.sort((a, b) => (a <= b ? 1 : -1))[k - 1];
 };
