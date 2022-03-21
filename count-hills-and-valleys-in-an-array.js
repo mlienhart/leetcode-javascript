757270861733fe4030744c52865e92cc291dc77e
Martin Lienhart <martin.lienhart@outlook.com>
2022-03-21 18:17:43 +0100

    count-hills-and-valleys-in-an-array

---
+++ count-hills-and-valleys-in-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countHillValley = function (nums) {
+  let a = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = "";
+    }
+  }
+
+  nums = nums.filter((x) => x !== "");
+
+  for (let i = 1; i < nums.length - 1; i++) {
+    if (
+      (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) ||
+      (nums[i - 1] > nums[i] && nums[i] < nums[i + 1])
+    ) {
+      a++;
+    }
+  }
+
+  return a;
+};
