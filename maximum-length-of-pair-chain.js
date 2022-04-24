990fa3d4b80bbc0385679afb860b26d5be09b53d
Martin Lienhart <martin.lienhart@outlook.com>
2022-04-24 14:54:16 +0200

    maximum-length-of-pair-chain

--- maximum-length-of-pair-chain.js
+++ maximum-length-of-pair-chain.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var findLongestChain = function (pairs) {
-  pairs.sort((a, b) => (a[1] < b[1] ? -1 : 1));
+  pairs.sort((a, b) => a[1] - b[1]);
 
   let a = 0;
   let b = -1000;
