53b3e89a227f34c8110c959d268f23e3d33c79c7
Martin Lienhart <martin.lienhart@outlook.com>
2022-04-26 21:37:10 +0200

    rearrange-words-in-a-sentence

--- rearrange-words-in-a-sentence.js
+++ rearrange-words-in-a-sentence.js
@@ -6,7 +6,7 @@ var arrangeWords = function (text) {
   let a = text
     .toLowerCase()
     .split(" ")
-    .sort((a, b) => (a.length < b.length ? -1 : 1));
+    .sort((a, b) => a.length - b.length);
   let b = a.slice(0, 1)[0].charAt(0).toUpperCase() + a.slice(0, 1)[0].slice(1);
 
   a.shift();
