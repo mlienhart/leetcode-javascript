a729c6a8414c273b08807220da330a6480b21736
Martin Lienhart <martin.lienhart@outlook.com>
2022-04-28 23:38:13 +0200

    second-largest-digit-in-a-string

--- second-largest-digit-in-a-string.js
+++ second-largest-digit-in-a-string.js
@@ -7,5 +7,5 @@ var secondHighest = function (s) {
   let b = a.split("");
   let c = new Set(b);
 
-  return c.size > 1 ? +[...c].sort((a, b) => (a > b ? -1 : 1))[1] : -1;
+  return c.size > 1 ? +[...c].sort((a, b) => b - a)[1] : -1;
 };
