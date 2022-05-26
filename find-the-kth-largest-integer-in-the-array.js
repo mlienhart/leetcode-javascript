a037977190b79d9ba2af1e9d4243fc83e37ced68
mlienhart <martin.lienhart@outlook.com>
2022-05-26 10:52:02 +0200

    find-the-kth-largest-integer-in-the-array

--- find-the-kth-largest-integer-in-the-array.js
+++ find-the-kth-largest-integer-in-the-array.js
@@ -5,7 +5,7 @@
  */
 var kthLargestNumber = function (nums, k) {
   let a = nums.map((x) => BigInt(x));
-  a.sort((a, b) => (a >= b ? -1 : 1));
+  a.sort((a, b) => (a <= b ? 1 : -1));
 
   return `${a[k - 1]}`;
 };
