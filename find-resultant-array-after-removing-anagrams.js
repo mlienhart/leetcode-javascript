e8b89e79a7e63db9bbe1c8c0bdf5d2536d5a190e
mlienhart <martin.lienhart@outlook.com>
2022-05-27 14:32:45 +0200

    find-resultant-array-after-removing-anagrams

--- find-resultant-array-after-removing-anagrams.js
+++ find-resultant-array-after-removing-anagrams.js
@@ -6,15 +6,15 @@ var removeAnagrams = function (words) {
   let a = [words[0]];
 
   for (let i = 0; i < words.length - 1; i++) {
-    let b = words[i]
-      .split("")
-      .sort((a, b) => (a > b ? 1 : -1))
-      .join("");
-    let c = words[i + 1]
-      .split("")
-      .sort((a, b) => (a > b ? 1 : -1))
-      .join("");
-    if (b !== c) {
+    let b = (c) => {
+      return c
+        .split("")
+        .sort((a, b) => (a > b ? 1 : -1))
+        .join("");
+    };
+    let d = b(words[i]);
+    let e = b(words[i + 1]);
+    if (d !== e) {
       a.push(words[i + 1]);
     }
   }
