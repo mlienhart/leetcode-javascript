d8627790c8206a13f0b3732fc7232bfbd53ae304
mlienhart <martin.lienhart@outlook.com>
2022-08-28 16:37:23 +0200

    two-sum

--- two-sum.js
+++ two-sum.js
@@ -4,14 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let a = [];
-
   for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = 1; j <= nums.length; j++) {
-      if (nums[i] + nums[j] === target && i !== j) {
-        a.push(i);
-        a.push(j);
-        return a;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
       }
     }
   }
