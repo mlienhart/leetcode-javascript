0d1c84f5708f7b19d9930b2da4c2ed2859046ea1
mlienhart <martin.lienhart@outlook.com>
2022-09-25 10:55:51 +0200

    sort-the-people

---
+++ sort-the-people.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string[]} names
+ * @param {number[]} heights
+ * @return {string[]}
+ */
+var sortPeople = function (names, heights) {
+  let a = [];
+  names.forEach((i) => a.push({ a: names[i], b: heights[i] }));
+  return a.sort((a, b) => b.b - a.b).map((x) => x.a);
+};
