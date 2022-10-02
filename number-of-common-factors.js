1fcee7d1692409fd210261567a396901fcc3622f
mlienhart <martin.lienhart@outlook.com>
2022-10-02 10:44:34 +0200

    number-of-common-factors

---
+++ number-of-common-factors.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var commonFactors = function (a, b) {
+  let x = a > b ? a : b;
+  let y = 0;
+
+  for (let i = 1; i <= x; i++) {
+    if (a % i === 0 && b % i === 0) {
+      y++;
+    }
+  }
+
+  return y;
+};
