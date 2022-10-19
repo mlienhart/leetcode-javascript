913a3fa9b47c4eb52a97121b5dda9eecd1076b7f
mlienhart <martin.lienhart@outlook.com>
2022-10-19 21:42:42 +0200

    longest-happy-prefix

--- longest-happy-prefix.js
+++ longest-happy-prefix.js
@@ -4,10 +4,10 @@
  */
 var longestPrefix = function (s) {
   for (let i = 1; i < s.length; i++) {
-    let b = s.slice(i);
-    let c = s.slice(0, s.length - i);
-    if (b === c) {
-      return b;
+    let a = s.slice(i);
+    let b = s.slice(0, s.length - i);
+    if (a === b) {
+      return a;
     }
   }
 
