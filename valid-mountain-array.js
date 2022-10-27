8c5bb3a0696d654fca43ab94b3a01d69a2e47e43
mlienhart <martin.lienhart@outlook.com>
2022-10-27 22:25:01 +0200

    valid-mountain-array

---
+++ valid-mountain-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var validMountainArray = function (arr) {
+  let a = 0;
+  let b = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      return false;
+    }
+    if (arr[i] < arr[i + 1]) {
+      a++;
+    }
+    if (arr[i] > arr[i + 1]) {
+      b++;
+      let c = arr.slice(i);
+      for (let i = 0; i < c.length; i++) {
+        if (c[i + 1] >= c[i]) {
+          return false;
+        }
+      }
+    }
+  }
+
+  return a >= 1 && b >= 1;
+};
