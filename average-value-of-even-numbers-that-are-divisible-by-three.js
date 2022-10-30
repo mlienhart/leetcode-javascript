9174331f423ffc964f0fe9ee2bec1005109b86f9
mlienhart <martin.lienhart@outlook.com>
2022-10-30 09:47:37 +0100

    average-value-of-even-numbers-that-are-divisible-by-three

--- average-value-of-even-numbers-that-are-divisible-by-three.js
+++ average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var averageValue = function (nums) {
-  let a = nums.filter((x) => x % 6 === 0);
+  const a = nums.filter((x) => x % 6 === 0);
 
   return a.length > 0 ? Math.floor(a.reduce((a, b) => a + b) / a.length) : 0;
 };
