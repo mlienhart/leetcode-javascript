1db105fde930aff21ae39075e08da0c8f690c17c
mlienhart <martin.lienhart@outlook.com>
2022-11-14 10:48:54 +0100

    number-of-subarrays-with-lcm-equal-to-k

--- number-of-subarrays-with-lcm-equal-to-k.js
+++ number-of-subarrays-with-lcm-equal-to-k.js
@@ -6,12 +6,10 @@
 var subarrayLCM = function (nums, k) {
   let a = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      let b = nums.slice(i, j + 1);
-      let c = b.every((x) => k % x === 0);
-      let d = b.some((x) => x === k);
-      if (c && d) {
+  for (let i = 0; i <= nums.length; i++) {
+    for (let j = 0; j <= nums.length; j++) {
+      let b = nums.slice(i, j);
+      if (b.every((x) => k % x === 0) && b.some((x) => x === k)) {
         a++;
       }
     }
