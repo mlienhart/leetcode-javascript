8a68ae3064a6ff2999658b9d86393050860d75ae
mlienhart <martin.lienhart@outlook.com>
2022-12-04 13:48:25 +0100

    unique-morse-code-words

---
+++ unique-morse-code-words.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function (words) {
+  const a = {
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+  };
+
+  return new Set(
+    words.map((x) =>
+      x
+        .split("")
+        .map((x) => a[x])
+        .join("")
+    )
+  ).size;
+};
