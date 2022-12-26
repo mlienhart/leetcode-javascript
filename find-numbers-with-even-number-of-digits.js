cf2def719c541ed9f7bf2a9e930802207e56e7d2
mlienhart <martin.lienhart@outlook.com>
2022-12-26 18:11:04 +0100

    find-numbers-with-even-number-of-digits

--- find-numbers-with-even-number-of-digits.js
+++ find-numbers-with-even-number-of-digits.js
@@ -3,13 +3,5 @@
  * @return {number}
  */
 var findNumbers = function (nums) {
-  let a = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i].toString().length % 2 === 0) {
-      a++;
-    }
-  }
-
-  return a;
+  return nums.filter((x) => x.toString().length % 2 === 0).length;
 };
