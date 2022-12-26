25721f5f9f1419365e99f4052303bcc24d21b9dc
mlienhart <martin.lienhart@outlook.com>
2022-12-26 16:07:48 +0100

    concatenation-of-array

--- concatenation-of-array.js
+++ concatenation-of-array.js
@@ -3,15 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function (nums) {
-  let a = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    a.push(nums[i]);
-  }
-
-  for (let i = 0; i < nums.length; i++) {
-    a.push(nums[i]);
-  }
-
-  return a;
+  return [...nums, ...nums];
 };
