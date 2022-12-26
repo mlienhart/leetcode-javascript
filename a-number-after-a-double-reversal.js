7ebe00ed5fdb35c6eff220c02aab902ae2d6ced8
mlienhart <martin.lienhart@outlook.com>
2022-12-26 15:49:07 +0100

    a-number-after-a-double-reversal

--- a-number-after-a-double-reversal.js
+++ a-number-after-a-double-reversal.js
@@ -3,9 +3,12 @@
  * @return {boolean}
  */
 var isSameAfterReversals = function (num) {
-  let a = `${num}`;
-  let b = a.split("").reverse().join("");
-  let c = (+b).toString().split("").reverse().join("");
-
-  return a === c;
+  return (
+    num.toString() ===
+    (+num.toString().split("").reverse().join(""))
+      .toString()
+      .split("")
+      .reverse()
+      .join("")
+  );
 };
