075b79bc06cfe5dd4a1c63e787c20aecee0f068b
mlienhart <martin.lienhart@outlook.com>
2022-12-26 22:23:13 +0100

    number-of-segments-in-a-string

--- number-of-segments-in-a-string.js
+++ number-of-segments-in-a-string.js
@@ -3,14 +3,5 @@
  * @return {number}
  */
 var countSegments = function (s) {
-  let a = s.split(" ");
-  let b = [];
-
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== "") {
-      b.push(a[i]);
-    }
-  }
-
-  return b.length;
+  return s.split(" ").filter(Boolean).length;
 };
