498425271f0a7098bc2e6443bb2247248dcb0b0f
mlienhart <martin.lienhart@outlook.com>
2022-12-26 16:15:39 +0100

    count-prefixes-of-a-given-string

--- count-prefixes-of-a-given-string.js
+++ count-prefixes-of-a-given-string.js
@@ -4,13 +4,5 @@
  * @return {number}
  */
 var countPrefixes = function (words, s) {
-  let a = 0;
-
-  for (let i = 0; i < words.length; i++) {
-    if (s.startsWith(words[i])) {
-      a++;
-    }
-  }
-
-  return a;
+  return words.filter((x) => s.startsWith(x)).length;
 };
