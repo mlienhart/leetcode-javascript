b123eba37c35de9c024f1394b24f2af3451dad4f
mlienhart <martin.lienhart@outlook.com>
2022-12-31 17:17:34 +0100

    reverse-prefix-of-word

--- reverse-prefix-of-word.js
+++ reverse-prefix-of-word.js
@@ -4,10 +4,11 @@
  * @return {string}
  */
 var reversePrefix = function (word, ch) {
-  let a = word.indexOf(ch);
-  let b = word.slice(0, a + 1);
-  let c = b.split("").reverse().join("");
-  let d = word.slice(a + 1);
-
-  return c + d;
+  return (
+    word
+      .slice(0, word.indexOf(ch) + 1)
+      .split("")
+      .reverse()
+      .join("") + word.slice(word.indexOf(ch) + 1)
+  );
 };
