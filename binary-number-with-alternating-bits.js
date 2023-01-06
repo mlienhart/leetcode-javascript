5f409a572a23b420c031f11269cdcd7cef09ffdd
mlienhart <martin.lienhart@outlook.com>
2023-01-06 22:00:22 +0100

    binary-number-with-alternating-bits

--- binary-number-with-alternating-bits.js
+++ binary-number-with-alternating-bits.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var hasAlternatingBits = function (n) {
-  const a = n.toString(2);
+  const binaryNumber = n.toString(2);
 
-  for (let i = 0; i < a.length - 1; i++) {
-    if (a[i] === a[i + 1]) {
+  for (let i = 0; i < binaryNumber.length - 1; i++) {
+    if (binaryNumber[i] === binaryNumber[i + 1]) {
       return false;
     }
   }
