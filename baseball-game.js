57b8e0a70b38e50d2953c5d1b65ead94e126d01a
mlienhart <martin.lienhart@outlook.com>
2023-01-06 21:21:04 +0100

    baseball-game

--- baseball-game.js
+++ baseball-game.js
@@ -3,22 +3,22 @@
  * @return {number}
  */
 var calPoints = function (ops) {
-  let a = [];
+  const result = [];
 
   for (let i = 0; i < ops.length; i++) {
     if (ops[i] !== "C" && ops[i] !== "D" && ops[i] !== "+") {
-      a.push(+ops[i]);
+      result.push(+ops[i]);
     }
     if (ops[i] === "C") {
-      a.pop();
+      result.pop();
     }
     if (ops[i] === "D") {
-      a.push(2 * a.slice(-1)[0]);
+      result.push(2 * result.slice(-1)[0]);
     }
     if (ops[i] === "+") {
-      a.push(a.slice(-1)[0] + +a.slice(-2)[0]);
+      result.push(result.slice(-1)[0] + +result.slice(-2)[0]);
     }
   }
 
-  return a.reduce((a, b) => a + b);
+  return result.reduce((a, b) => a + b, 0);
 };
