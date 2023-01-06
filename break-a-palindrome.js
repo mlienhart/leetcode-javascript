d023363a7ea5fd0ac2f65e5edff8c52ff2d82767
mlienhart <martin.lienhart@outlook.com>
2023-01-06 22:16:24 +0100

    break-a-palindrome

--- break-a-palindrome.js
+++ break-a-palindrome.js
@@ -3,19 +3,19 @@
  * @return {string}
  */
 var breakPalindrome = function (palindrome) {
-  let a = palindrome.split("");
+  let result = palindrome.split("");
 
-  for (let i = 0; i < Math.floor(a.length / 2); i++) {
-    if (a[i] !== "a") {
-      a[i] = "a";
-      return a.join("");
+  for (let i = 0; i < Math.floor(result.length / 2); i++) {
+    if (result[i] !== "a") {
+      result[i] = "a";
+      return result.join("");
     }
   }
 
-  if (a.length === 1) {
+  if (result.length === 1) {
     return "";
   } else {
-    a[a.length - 1] = "b";
-    return a.join("");
+    result[result.length - 1] = "b";
+    return result.join("");
   }
 };
