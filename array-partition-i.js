50b049bfec1fd49f30584691068e60c96c571b64
mlienhart <martin.lienhart@outlook.com>
2023-01-06 10:52:56 +0100

    array-partition-i

--- array-partition-i.js
+++ array-partition-i.js
@@ -4,12 +4,12 @@
  */
 var arrayPairSum = function (nums) {
   nums.sort((a, b) => a - b);
-  let a = [];
+  const result = [];
 
   for (let i = 0; i < nums.length; i++) {
-    a.push(Math.min(nums[i], nums[i + 1]));
+    result.push(Math.min(nums[i], nums[i + 1]));
     i++;
   }
 
-  return a.reduce((a, b) => a + b, 0);
+  return result.reduce((a, b) => a + b, 0);
 };
