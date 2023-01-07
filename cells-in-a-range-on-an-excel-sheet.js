745d07eb09fb03d9d66c631e2831779d2e586c7e
mlienhart <martin.lienhart@outlook.com>
2023-01-07 11:14:41 +0100

    cells-in-a-range-on-an-excel-sheet

--- cells-in-a-range-on-an-excel-sheet.js
+++ cells-in-a-range-on-an-excel-sheet.js
@@ -3,13 +3,13 @@
  * @return {string[]}
  */
 var cellsInRange = function (s) {
-  let a = [];
+  const result = [];
 
   for (let i = s.charCodeAt(0); i <= s.charCodeAt(3); i++) {
     for (let j = s[1]; j <= s[4]; j++) {
-      a.push(String.fromCharCode(i) + j);
+      result.push(String.fromCharCode(i) + j);
     }
   }
 
-  return a;
+  return result;
 };
