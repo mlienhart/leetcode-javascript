2b4460aa90086466fbc2d67efeabeda75e484ea8
mlienhart <martin.lienhart@outlook.com>
2023-01-14 14:52:57 +0100

    climbing-stairs

--- climbing-stairs.js
+++ climbing-stairs.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  let a = [0, 1, 2, 3];
+  const result = [0, 1, 2, 3];
 
   for (let i = 4; i <= n; i++) {
-    a.push(a[i - 1] + a[i - 2]);
+    result.push(result[i - 1] + result[i - 2]);
   }
 
-  return a[n];
+  return result[n];
 };
