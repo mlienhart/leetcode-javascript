224f928e86d55b5561d15f1fa726837477ad0386
mlienhart <martin.lienhart@outlook.com>
2023-02-05 17:35:46 +0100

    count-the-number-of-consistent-strings

--- count-the-number-of-consistent-strings.js
+++ count-the-number-of-consistent-strings.js
@@ -4,14 +4,13 @@
  * @return {number}
  */
 var countConsistentStrings = function (allowed, words) {
-  let a = allowed.split("");
-  let b = 0;
+  let result = 0;
 
   for (let i = 0; i < words.length; i++) {
-    if (words[i].split("").every((x) => a.includes(x))) {
-      b++;
+    if (words[i].split("").every((x) => allowed.split("").includes(x))) {
+      result++;
     }
   }
 
-  return b;
+  return result;
 };
