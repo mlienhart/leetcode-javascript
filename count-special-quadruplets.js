1616bb8eaf9802415e099a267add1042bb528548
mlienhart <martin.lienhart@outlook.com>
2023-02-05 17:28:17 +0100

    count-special-quadruplets

--- count-special-quadruplets.js
+++ count-special-quadruplets.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var countQuadruplets = function (nums) {
-  let a = 0;
+  let result = 0;
 
   for (let i = 0; i < nums.length - 3; i++) {
     for (let j = i + 1; j < nums.length - 2; j++) {
       for (let k = j + 1; k < nums.length - 1; k++) {
         for (let l = k + 1; l < nums.length; l++) {
           if (nums[i] + nums[j] + nums[k] === nums[l]) {
-            a++;
+            result++;
           }
         }
       }
     }
   }
 
-  return a;
+  return result;
 };
