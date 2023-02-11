12f22d731a8c9aebfa6543775828bdce40d45b3f
mlienhart <martin.lienhart@outlook.com>
2023-02-11 12:37:41 +0100

    day-of-the-year

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -3,8 +3,7 @@
  * @return {number}
  */
 var dayOfYear = function (date) {
-  let a = new Date(date);
-  let b = new Date(date.slice(0, 4));
-
-  return (a - b) / (1000 * 60 * 60 * 24) + 1;
+  return (
+    (new Date(date) - new Date(date.slice(0, 4))) / (1000 * 60 * 60 * 24) + 1
+  );
 };
