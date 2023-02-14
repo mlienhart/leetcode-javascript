b9b72a5b9cb00e2b0cc0bd2d957ebbcc9d3074fb
mlienhart <martin.lienhart@outlook.com>
2023-02-14 13:35:34 +0100

    equal-row-and-column-pairs

--- equal-row-and-column-pairs.js
+++ equal-row-and-column-pairs.js
@@ -3,28 +3,28 @@
  * @return {number}
  */
 var equalPairs = function (grid) {
-  const a = [];
+  const transposedMatrix = [];
 
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid.length; j++) {
-      if (!a[i]) {
-        a.push([]);
+      if (!transposedMatrix[i]) {
+        transposedMatrix.push([]);
       }
-      a[i].push(grid[j][i]);
+      transposedMatrix[i].push(grid[j][i]);
     }
   }
 
-  const b = grid.map((x) => x.join("-"));
-  const c = a.map((x) => x.join("-"));
-  let d = 0;
+  const originalMappedMatrix = grid.map((x) => x.join("-"));
+  const transposedMappedMatrix = transposedMatrix.map((x) => x.join("-"));
+  let result = 0;
 
-  for (let i = 0; i < b.length; i++) {
-    for (let j = 0; j < c.length; j++) {
-      if (b[i] === c[j]) {
-        d++;
+  for (let i = 0; i < originalMappedMatrix.length; i++) {
+    for (let j = 0; j < transposedMappedMatrix.length; j++) {
+      if (originalMappedMatrix[i] === transposedMappedMatrix[j]) {
+        result++;
       }
     }
   }
 
-  return d;
+  return result;
 };
