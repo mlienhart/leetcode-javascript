f64e36edcfdbe90088132f172c831cb9c16b1e0c
mlienhart <martin.lienhart@outlook.com>
2023-02-17 09:18:26 +0100

    fibonacci-number

--- fibonacci-number.js
+++ fibonacci-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var fib = function (n) {
-  let a = [0, 1];
+  const result = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    a.push(a[i - 2] + a[i - 1]);
+    result.push(result[i - 2] + result[i - 1]);
   }
 
-  return a[n];
+  return result[n];
 };
