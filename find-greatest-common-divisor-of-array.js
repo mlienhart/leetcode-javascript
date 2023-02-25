65910882a9701180add7e62530620aeb280ebe94
mlienhart <martin.lienhart@outlook.com>
2023-02-25 22:35:26 +0100

    find-greatest-common-divisor-of-array

--- find-greatest-common-divisor-of-array.js
+++ find-greatest-common-divisor-of-array.js
@@ -3,13 +3,8 @@
  * @return {number}
  */
 var findGCD = function (nums) {
-  let a = [...nums].sort((a, b) => a - b);
-  let b = [...nums].sort((a, b) => b - a);
-  let c = a[0];
-  let d = b[0];
-
-  for (let i = c; i >= 0; i--) {
-    if (c % i === 0 && d % i === 0) {
+  for (let i = Math.min(...nums); i >= 0; i--) {
+    if (Math.min(...nums) % i === 0 && Math.max(...nums) % i === 0) {
       return i;
     }
   }
