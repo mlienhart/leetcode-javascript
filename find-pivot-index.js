3e90ceee14ff98bb42eee6831677fbd0613a1ae9
mlienhart <martin.lienhart@outlook.com>
2023-02-25 23:00:41 +0100

    find-pivot-index

--- find-pivot-index.js
+++ find-pivot-index.js
@@ -4,9 +4,10 @@
  */
 var pivotIndex = function (nums) {
   for (let i = 0; i < nums.length; i++) {
-    let a = nums.slice(0, i).reduce((a, b) => a + b, 0);
-    let b = nums.slice(i + 1).reduce((a, b) => a + b, 0);
-    if (a === b) {
+    if (
+      nums.slice(0, i).reduce((a, b) => a + b, 0) ===
+      nums.slice(i + 1).reduce((a, b) => a + b, 0)
+    ) {
       return i;
     }
   }
