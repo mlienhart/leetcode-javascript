0a3858f89d421af6ce3df6aed6508dc80a7e8a48
mlienhart <martin.lienhart@outlook.com>
2023-02-25 22:53:15 +0100

    find-lucky-integer-in-an-array

--- find-lucky-integer-in-an-array.js
+++ find-lucky-integer-in-an-array.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var findLucky = function (arr) {
-  let a = [];
-  let b = new Map();
+  const result = [];
+  const counts = new Map();
 
   for (let i = 0; i < arr.length; i++) {
-    b.set(arr[i], (b.get(arr[i]) || 0) + 1);
+    counts.set(arr[i], (counts.get(arr[i]) || 0) + 1);
   }
 
-  for (let [key, value] of b.entries()) {
+  for (const [key, value] of counts.entries()) {
     if (key === value) {
-      a.push(value);
+      result.push(value);
     }
   }
 
-  return a.length > 0 ? Math.max(...a) : -1;
+  return result.length > 0 ? Math.max(...result) : -1;
 };
