59a17f9aebeadd701aac05837272c426ce8730c2
mlienhart <martin.lienhart@outlook.com>
2023-03-09 08:10:34 +0100

    find-triangular-sum-of-an-array

--- find-triangular-sum-of-an-array.js
+++ find-triangular-sum-of-an-array.js
@@ -4,13 +4,11 @@
  */
 var triangularSum = function (nums) {
   while (nums.length !== 1) {
-    let a = [];
-
+    const result = [];
     for (let i = 0; i < nums.length - 1; i++) {
-      a.push((nums[i] + nums[i + 1]) % 10);
+      result.push((nums[i] + nums[i + 1]) % 10);
     }
-
-    nums = a;
+    nums = result;
   }
 
   return nums[0];
