198192cd0e28c53b8bb46890ccaaa09007825ae0
mlienhart <martin.lienhart@outlook.com>
2023-03-14 22:26:48 +0100

    fizz-buzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -3,19 +3,19 @@
  * @return {string[]}
  */
 var fizzBuzz = function (n) {
-  let a = [];
+  const result = [];
 
   for (let i = 1; i <= n; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      a.push("FizzBuzz");
+      result.push("FizzBuzz");
     } else if (i % 3 === 0) {
-      a.push("Fizz");
+      result.push("Fizz");
     } else if (i % 5 === 0) {
-      a.push("Buzz");
+      result.push("Buzz");
     } else {
-      a.push(i.toString());
+      result.push(i.toString());
     }
   }
 
-  return a;
+  return result;
 };
