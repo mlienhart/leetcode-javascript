aec286f87ba686437a21588e731f365a4d1b2398
mlienhart <martin.lienhart@outlook.com>
2023-03-26 15:55:58 +0200

    intersection-of-two-arrays-ii

--- intersection-of-two-arrays-ii.js
+++ intersection-of-two-arrays-ii.js
@@ -4,17 +4,17 @@
  * @return {number[]}
  */
 var intersect = function (nums1, nums2) {
-  let a = [];
+  const result = [];
 
   for (let i = 0; i < nums1.length; i++) {
     for (let j = 0; j < nums2.length; j++) {
       if (nums1[i] === nums2[j]) {
-        a.push(nums1[i]);
+        result.push(nums1[i]);
         nums1[i] = null;
         nums2[j] = null;
       }
     }
   }
 
-  return a.filter((x) => x !== null);
+  return result.filter((x) => x !== null);
 };
