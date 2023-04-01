2658bc932a8849b1aa7395714ab34f247a2790f1
mlienhart <martin.lienhart@outlook.com>
2023-04-01 23:50:29 +0200

    form-smallest-number-from-two-digit-arrays

---
+++ form-smallest-number-from-two-digit-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var minNumber = function (nums1, nums2) {
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+
+  return (
+    nums1.filter((x) => nums2.includes(x))[0] ??
+    +[nums1[0], nums2[0]].sort((a, b) => a - b).join("")
+  );
+};
