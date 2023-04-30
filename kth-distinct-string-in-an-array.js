856df51f9b3a417a9663a3a5ab2c2ef383d7a87d
mlienhart <martin.lienhart@outlook.com>
2023-04-30 09:33:50 +0200

    kth-distinct-string-in-an-array

--- kth-distinct-string-in-an-array.js
+++ kth-distinct-string-in-an-array.js
@@ -4,18 +4,18 @@
  * @return {string}
  */
 var kthDistinct = function (arr, k) {
-  let a = new Map();
-  let b = [];
+  const map = new Map();
+  const result = [];
 
   for (let i = 0; i < arr.length; i++) {
-    a.set(arr[i], (a.get(arr[i]) || 0) + 1);
+    map.set(arr[i], (map.get(arr[i]) || 0) + 1);
   }
 
-  for (let [key, value] of a.entries()) {
+  for (let [key, value] of map.entries()) {
     if (value === 1) {
-      b.push(key);
+      result.push(key);
     }
   }
 
-  return b[k - 1] || "";
+  return result[k - 1] || "";
 };
