540a47fb4d92f5102a21022e2bc06cf8fdd077a6
mlienhart <martin.lienhart@outlook.com>
2023-05-12 17:14:42 +0200

    largest-3-same-digit-number-in-string

--- largest-3-same-digit-number-in-string.js
+++ largest-3-same-digit-number-in-string.js
@@ -5,7 +5,7 @@
 var largestGoodInteger = function (num) {
   const result = [];
 
-  for (let i = 0; i < num.length; i++) {
+  for (let i = 0; i < num.length - 2; i++) {
     if (num[i] === num[i + 1] && num[i + 1] === num[i + 2]) {
       result.push(
         num
