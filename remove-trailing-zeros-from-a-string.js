06fd71618be71594efe3ece78c16ad3f3990df3b
mlienhart <martin.lienhart@outlook.com>
2023-05-28 14:44:28 +0200

    remove-trailing-zeros-from-a-string

---
+++ remove-trailing-zeros-from-a-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function (num) {
+  while (num[num.length - 1] === "0") {
+    num = num.substring(0, num.length - 1);
+  }
+
+  return num;
+};
