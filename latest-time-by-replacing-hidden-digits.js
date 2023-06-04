a5744f21f4554b2e17256ad19c99c8fc93d815ff
mlienhart <martin.lienhart@outlook.com>
2023-06-04 22:19:16 +0200

    latest-time-by-replacing-hidden-digits

--- latest-time-by-replacing-hidden-digits.js
+++ latest-time-by-replacing-hidden-digits.js
@@ -3,20 +3,20 @@
  * @return {string}
  */
 var maximumTime = function (time) {
-  let a = time.split("");
+  let result = time.split("");
 
-  if (a[0] === "?") {
-    a[0] = a[1] > 3 ? "1" : "2";
+  if (result[0] === "?") {
+    result[0] = result[1] > 3 ? "1" : "2";
   }
-  if (a[1] === "?") {
-    a[1] = a[0] > 1 ? "3" : "9";
+  if (result[1] === "?") {
+    result[1] = result[0] > 1 ? "3" : "9";
   }
-  if (a[3] === "?") {
-    a[3] = "5";
+  if (result[3] === "?") {
+    result[3] = "5";
   }
-  if (a[4] === "?") {
-    a[4] = "9";
+  if (result[4] === "?") {
+    result[4] = "9";
   }
 
-  return a.join("");
+  return result.join("");
 };
