b761847e4cac4606b9bbcd0579f1feff9e42c231
mlienhart <martin.lienhart@outlook.com>
2023-06-11 13:09:57 +0200

    longer-contiguous-segments-of-ones-than-zeros

--- longer-contiguous-segments-of-ones-than-zeros.js
+++ longer-contiguous-segments-of-ones-than-zeros.js
@@ -3,8 +3,8 @@
  * @return {boolean}
  */
 var checkZeroOnes = function (s) {
-  let a = s.split("0").sort((a, b) => (a < b ? 1 : -1));
-  let b = s.split("1").sort((a, b) => (a < b ? 1 : -1));
-
-  return a[0].length > b[0].length;
+  return (
+    s.split("0").sort((a, b) => (a < b ? 1 : -1))[0].length >
+    s.split("1").sort((a, b) => (a < b ? 1 : -1))[0].length
+  );
 };
