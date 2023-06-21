4c0e44454424bbc80901d0cb2039f06b13a5b490
mlienhart <martin.lienhart@outlook.com>
2023-06-21 12:34:13 +0200

    longest-continuous-increasing-subsequence

--- longest-continuous-increasing-subsequence.js
+++ longest-continuous-increasing-subsequence.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var findLengthOfLCIS = function (nums) {
-  let a = 0;
-  let b = 0;
+  let result = 0;
+  let count = 0;
 
   for (let i = 0; i < nums.length; i++) {
     if (i === 0 || nums[i - 1] < nums[i]) {
-      a = Math.max(a, ++b);
+      result = Math.max(result, ++count);
     } else {
-      b = 1;
+      count = 1;
     }
   }
 
-  return a;
+  return result;
 };
