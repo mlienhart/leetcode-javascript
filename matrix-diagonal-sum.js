b744642e914152aa81bff029ef9020563e051250
mlienhart <martin.lienhart@outlook.com>
2023-07-09 12:51:31 +0200

    matrix-diagonal-sum

--- matrix-diagonal-sum.js
+++ matrix-diagonal-sum.js
@@ -3,27 +3,25 @@
  * @return {number}
  */
 var diagonalSum = function (mat) {
-  let a = [];
+  const result = [];
 
   for (let i = 0; i < mat.length; i++) {
-    a.push(mat[i][i]);
+    result.push(mat[i][i]);
   }
 
   mat.reverse();
 
   if (mat.length % 2 === 0) {
     for (let i = 0; i < mat.length; i++) {
-      a.push(mat[i][i]);
+      result.push(mat[i][i]);
     }
-  }
-
-  if (mat.length % 2 !== 0) {
+  } else {
     for (let i = 0; i < mat.length; i++) {
       if (i !== Math.floor(mat.length / 2)) {
-        a.push(mat[i][i]);
+        result.push(mat[i][i]);
       }
     }
   }
 
-  return a.reduce((a, b) => a + b, 0);
+  return result.reduce((a, b) => a + b, 0);
 };
