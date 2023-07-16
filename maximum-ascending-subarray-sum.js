aeca2f65a7bd28796581f49e37b611bf66eefe5d
mlienhart <martin.lienhart@outlook.com>
2023-07-16 23:51:29 +0200

    maximum-ascending-subarray-sum

--- maximum-ascending-subarray-sum.js
+++ maximum-ascending-subarray-sum.js
@@ -1,14 +1,14 @@
 var maxAscendingSum = function (nums) {
-  let a = nums[0];
-  let b = nums[0];
+  let sum = nums[0];
+  let result = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
     if (nums[i] <= nums[i - 1]) {
-      a = 0;
+      sum = 0;
     }
-    a = a + nums[i];
-    b = Math.max(b, a);
+    sum = sum + nums[i];
+    result = Math.max(result, sum);
   }
 
-  return b;
+  return result;
 };
