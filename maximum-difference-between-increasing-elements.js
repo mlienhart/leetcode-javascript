e255075dfeceaf071516578dfe786797e2c04724
mlienhart <martin.lienhart@outlook.com>
2023-07-18 23:16:09 +0200

    maximum-difference-between-increasing-elements

--- maximum-difference-between-increasing-elements.js
+++ maximum-difference-between-increasing-elements.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var maximumDifference = function (nums) {
-  let a = -1;
+  let result = -1;
 
   for (let i = 0; i < nums.length - 1; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] < nums[j]) {
-        a = Math.max(a, nums[j] - nums[i]);
+        result = Math.max(result, nums[j] - nums[i]);
       }
     }
   }
 
-  return a;
+  return result;
 };
