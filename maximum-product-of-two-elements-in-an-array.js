a2489abfbaedaf4ff20d0d55442e3a3ce14d5b38
mlienhart <martin.lienhart@outlook.com>
2023-08-15 09:49:15 +0200

    maximum-product-of-two-elements-in-an-array

--- maximum-product-of-two-elements-in-an-array.js
+++ maximum-product-of-two-elements-in-an-array.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-  let a = nums.sort((a, b) => b - a);
+  nums.sort((a, b) => b - a);
 
-  return (a[0] - 1) * (a[1] - 1);
+  return (nums[0] - 1) * (nums[1] - 1);
 };
