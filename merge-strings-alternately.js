2668ca3fcfea7906276a61c33c73f11b0d123b22
mlienhart <martin.lienhart@outlook.com>
2023-08-20 10:12:31 +0200

    merge-strings-alternately

--- merge-strings-alternately.js
+++ merge-strings-alternately.js
@@ -4,11 +4,11 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-  let a = [];
+  const result = [];
 
   for (i = 0; i < 100; i++) {
-    a.push(word1[i]), a.push(word2[i]);
+    result.push(word1[i]), result.push(word2[i]);
   }
 
-  return a.join("");
+  return result.join("");
 };
