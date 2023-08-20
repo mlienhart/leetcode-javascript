a5bda4406292c88e136d90a21cf10f6ea6f59172
mlienhart <martin.lienhart@outlook.com>
2023-08-20 09:26:47 +0200

    maximum-repeating-substring

--- maximum-repeating-substring.js
+++ maximum-repeating-substring.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var maxRepeating = function (sequence, word) {
-  let a = 0;
-  let b = word;
+  let result = 0;
+  let currentWord = word;
 
-  while (sequence.includes(b)) {
-    a++;
-    b = b + word;
+  while (sequence.includes(currentWord)) {
+    result++;
+    currentWord = currentWord + word;
   }
 
-  return a;
+  return result;
 };
