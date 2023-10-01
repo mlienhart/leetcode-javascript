2b52108148f70f862b2c38961dcbfdcb85316f79
mlienhart <martin.lienhart@outlook.com>
2023-10-01 11:06:03 +0200

    minimum-number-of-moves-to-seat-everyone

--- minimum-number-of-moves-to-seat-everyone.js
+++ minimum-number-of-moves-to-seat-everyone.js
@@ -6,11 +6,11 @@
 var minMovesToSeat = function (seats, students) {
   seats.sort((a, b) => a - b);
   students.sort((a, b) => a - b);
-  let a = 0;
+  let result = 0;
 
   for (let i = 0; i < seats.length; i++) {
-    a = a + Math.abs(seats[i] - students[i]);
+    result = result + Math.abs(seats[i] - students[i]);
   }
 
-  return a;
+  return result;
 };
