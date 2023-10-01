bc60781a274bb746025f9577f40bda91d1ad07f3
mlienhart <martin.lienhart@outlook.com>
2023-10-01 10:34:23 +0200

    maximum-value-of-an-ordered-triplet-i

---
+++ maximum-value-of-an-ordered-triplet-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumTripletValue = function (nums) {
+  let result = 0;
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const maximumValue = Math.max((nums[i] - nums[j]) * nums[k], 0);
+        if (maximumValue > result) {
+          result = maximumValue;
+        }
+      }
+    }
+  }
+
+  return result;
+};
