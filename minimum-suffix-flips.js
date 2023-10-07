df4b5dd8c691f40ee28a45b1c5402c5c819673ee
mlienhart <martin.lienhart@outlook.com>
2023-10-07 09:29:43 +0200

    minimum-suffix-flips

--- minimum-suffix-flips.js
+++ minimum-suffix-flips.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var minFlips = function (target) {
-  let a = `${BigInt(target)}`;
-  let b = 0;
+  const targetStringValue = `${BigInt(target)}`;
+  let result = 0;
 
-  if (a === "0") {
+  if (targetStringValue === "0") {
     return 0;
   }
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== a[i + 1]) {
-      b++;
+  for (let i = 0; i < targetStringValue.length; i++) {
+    if (targetStringValue[i] !== targetStringValue[i + 1]) {
+      result++;
     }
   }
 
-  return b;
+  return result;
 };
