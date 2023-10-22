eda57d6d0bb193086876309eff29172aae9a90dc
mlienhart <martin.lienhart@outlook.com>
2023-10-22 09:50:20 +0200

    monotonic-array

--- monotonic-array.js
+++ monotonic-array.js
@@ -3,20 +3,17 @@
  * @return {boolean}
  */
 var isMonotonic = function (nums) {
-  let a = 0;
-  let b = 0;
+  let firstResult = 0;
+  let secondResult = 0;
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] >= nums[i + 1]) {
-      a++;
+      firstResult++;
     }
-  }
-
-  for (let i = 0; i < nums.length; i++) {
     if (nums[i] <= nums[i + 1]) {
-      b++;
+      secondResult++;
     }
   }
 
-  return a === nums.length - 1 || b === nums.length - 1;
+  return firstResult === nums.length - 1 || secondResult === nums.length - 1;
 };
