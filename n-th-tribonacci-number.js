1197acd48d0916a29fffbecab5ed3262b93439c9
mlienhart <martin.lienhart@outlook.com>
2023-11-04 16:46:27 +0100

    n-th-tribonacci-number

--- n-th-tribonacci-number.js
+++ n-th-tribonacci-number.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var tribonacci = function (n) {
-  const a = [0, 1, 1];
+  const result = [0, 1, 1];
 
   for (let i = 3; i <= n; i++) {
-    a.push(a[i - 1] + a[i - 2] + a[i - 3]);
+    result.push(result[i - 1] + result[i - 2] + result[i - 3]);
   }
 
-  return a[n];
+  return result[n];
 };
