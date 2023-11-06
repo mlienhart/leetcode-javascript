74209fa4f86b1257b61fad0e5809c28bf4fc62af
mlienhart <martin.lienhart@outlook.com>
2023-11-06 21:58:45 +0100

    number-of-arithmetic-triplets

--- number-of-arithmetic-triplets.js
+++ number-of-arithmetic-triplets.js
@@ -4,22 +4,17 @@
  * @return {number}
  */
 var arithmeticTriplets = function (nums, diff) {
-  let a = 0;
+  let result = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      for (let k = 0; k < nums.length; k++) {
-        if (
-          i < j &&
-          j < k &&
-          nums[j] - nums[i] == diff &&
-          nums[k] - nums[j] == diff
-        ) {
-          a++;
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff) {
+          result++;
         }
       }
     }
   }
 
-  return a;
+  return result;
 };
