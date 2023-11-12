7c94b8a48e4696331bdb379790e7e6c35001dbea
mlienhart <martin.lienhart@outlook.com>
2023-11-12 21:43:31 +0100

    distribute-candies-among-children-i

---
+++ distribute-candies-among-children-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+var distributeCandies = function (n, limit) {
+  let result = 0;
+
+  for (let i = 0; i <= limit; i++) {
+    for (let j = 0; j <= limit; j++) {
+      for (let k = 0; k <= limit; k++) {
+        if (i + j + k === n) {
+          result++;
+        }
+      }
+    }
+  }
+
+  return result;
+};
