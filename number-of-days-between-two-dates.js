176cb14141d15480c875b75f11885866d89f7f91
mlienhart <martin.lienhart@outlook.com>
2023-11-13 22:15:36 +0100

    number-of-days-between-two-dates

--- number-of-days-between-two-dates.js
+++ number-of-days-between-two-dates.js
@@ -4,11 +4,7 @@
  * @return {number}
  */
 var daysBetweenDates = function (date1, date2) {
-  let a = 24 * 60 * 60 * 1000;
-
-  if (date2 > date1) {
-    return (new Date(date2) - new Date(date1)) / a;
-  }
-
-  return (new Date(date1) - new Date(date2)) / a;
+  return date2 > date1
+    ? (new Date(date2) - new Date(date1)) / (24 * 60 * 60 * 1000)
+    : (new Date(date1) - new Date(date2)) / (24 * 60 * 60 * 1000);
 };
