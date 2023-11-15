591b04f79b6b878db2285ad30c181a4f450d8d0a
mlienhart <martin.lienhart@outlook.com>
2023-11-15 21:38:23 +0100

    number-of-pairs-of-strings-with-concatenation-equal-to-target

--- number-of-pairs-of-strings-with-concatenation-equal-to-target.js
+++ number-of-pairs-of-strings-with-concatenation-equal-to-target.js
@@ -4,15 +4,15 @@
  * @return {number}
  */
 var numOfPairs = function (nums, target) {
-  let a = 0;
+  let result = 0;
 
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
       if (i !== j && nums[i] + nums[j] === target) {
-        a++;
+        result++;
       }
     }
   }
 
-  return a;
+  return result;
 };
