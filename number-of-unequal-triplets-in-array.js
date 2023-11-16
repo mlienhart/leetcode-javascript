eec32e6ec004b759c17277eb2bf0072509efd949
mlienhart <martin.lienhart@outlook.com>
2023-11-16 22:27:18 +0100

    number-of-unequal-triplets-in-array

--- number-of-unequal-triplets-in-array.js
+++ number-of-unequal-triplets-in-array.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var unequalTriplets = function (nums) {
-  let a = 0;
+  let result = 0;
 
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       for (let k = j + 1; k < nums.length; k++) {
         if (nums[i] !== nums[j] && nums[i] !== nums[k] && nums[j] !== nums[k]) {
-          a++;
+          result++;
         }
       }
     }
   }
 
-  return a;
+  return result;
 };
