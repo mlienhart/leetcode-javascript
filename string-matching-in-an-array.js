7a8a59adc52dd0c13f68b7635071937ca578c084
mlienhart <martin.lienhart@outlook.com>
2024-02-01 23:01:18 +0100

    string-matching-in-an-array

--- string-matching-in-an-array.js
+++ string-matching-in-an-array.js
@@ -3,18 +3,18 @@
  * @return {string[]}
  */
 var stringMatching = function (words) {
-  let a = new Set();
+  const set = new Set();
 
   for (let i = 0; i < words.length - 1; i++) {
     for (let j = i + 1; j < words.length; j++) {
       if (words[i].includes(words[j])) {
-        a.add(words[j]);
+        set.add(words[j]);
       }
       if (words[j].includes(words[i])) {
-        a.add(words[i]);
+        set.add(words[i]);
       }
     }
   }
 
-  return [...a];
+  return [...set];
 };
