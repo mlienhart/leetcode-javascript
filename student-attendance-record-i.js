679d8053b6ec0f982a512c03d93d3a0b68a88aba
mlienhart <martin.lienhart@outlook.com>
2024-02-25 10:56:12 +0100

    student-attendance-record-i

--- student-attendance-record-i.js
+++ student-attendance-record-i.js
@@ -3,20 +3,19 @@
  * @return {boolean}
  */
 var checkRecord = function (s) {
-  let a = s.split("");
-  let b = 0;
+  let result = 0;
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === "A") {
-      b++;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "A") {
+      result++;
     }
   }
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === "L" && a[i + 1] === "L" && a[i + 2] === "L") {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "L" && s[i + 1] === "L" && s[i + 2] === "L") {
       return false;
     }
   }
 
-  return b < 2;
+  return result < 2;
 };
