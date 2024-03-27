7a23ee4beae55bcf178ed02bab3d0067b34ed273
mlienhart <martin.lienhart@outlook.com>
2024-03-27 14:43:23 +0100

    two-out-of-three

--- two-out-of-three.js
+++ two-out-of-three.js
@@ -5,31 +5,31 @@
  * @return {number[]}
  */
 var twoOutOfThree = function (nums1, nums2, nums3) {
-  let a = [];
+  const result = [];
 
   for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0; j < +nums2.length; j++) {
+    for (let j = 0; j < nums2.length; j++) {
       if (nums1[i] === nums2[j]) {
-        a.push(nums1[i]);
+        result.push(nums1[i]);
       }
     }
   }
 
   for (let i = 0; i < nums2.length; i++) {
-    for (let j = 0; j < +nums3.length; j++) {
+    for (let j = 0; j < nums3.length; j++) {
       if (nums2[i] === nums3[j]) {
-        a.push(nums2[i]);
+        result.push(nums2[i]);
       }
     }
   }
 
   for (let i = 0; i < nums3.length; i++) {
-    for (let j = 0; j < +nums1.length; j++) {
+    for (let j = 0; j < nums1.length; j++) {
       if (nums3[i] === nums1[j]) {
-        a.push(nums3[i]);
+        result.push(nums3[i]);
       }
     }
   }
 
-  return [...new Set([...a])];
+  return [...new Set([...result])];
 };
