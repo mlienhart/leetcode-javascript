8d04c8ace66f31d1d4f10e6149242f109c49b15c
mlienhart <martin.lienhart@outlook.com>
2024-07-16 23:40:33 +0200

    remove-digit-from-number-to-maximize-result

--- remove-digit-from-number-to-maximize-result.js
+++ remove-digit-from-number-to-maximize-result.js
@@ -4,16 +4,16 @@
  * @return {string}
  */
 var removeDigit = function (number, digit) {
-  let a = "0";
+  let result = "0";
 
   for (let i = 0; i < number.length; i++) {
     if (number[i] === digit) {
-      let b = number.slice(0, i) + number.slice(i + 1);
-      if (b > a) {
-        a = b;
+      const sumWithoutCurrentValue = number.slice(0, i) + number.slice(i + 1);
+      if (sumWithoutCurrentValue > result) {
+        result = sumWithoutCurrentValue;
       }
     }
   }
 
-  return a;
+  return result;
 };
