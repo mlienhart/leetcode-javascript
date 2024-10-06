740e96ae7aac915011a1a59c03514309a7c18ff9
mlienhart <martin.lienhart@outlook.com>
2024-10-06 09:34:46 +0200

    truncate-sentence

--- truncate-sentence.js
+++ truncate-sentence.js
@@ -4,14 +4,5 @@
  * @return {string}
  */
 var truncateSentence = function (s, k) {
-  let a = s.split(" ");
-  let arr = [];
-
-  for (let i = 0; i < a.length; i++) {
-    if (i < k) {
-      arr.push(a[i]);
-    }
-  }
-
-  return arr.join(" ");
+  return s.split(" ").slice(0, k).join(" ");
 };
