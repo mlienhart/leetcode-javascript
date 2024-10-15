77e7c246a631493e49eff9717965616c7e6c2757
mlienhart <martin.lienhart@outlook.com>
2024-10-15 20:34:09 +0200

    squares-of-a-sorted-array

--- squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var sortedSquares = function (nums) {
-  const result = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    result.push(nums[i] * nums[i]);
-  }
-
-  return result.sort((a, b) => a - b);
+  return nums.map((x) => x * x).sort((a, b) => a - b);
 };
