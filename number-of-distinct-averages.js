3a881c86f10caac4a821b7e5d52b5f870f573ce7
mlienhart <martin.lienhart@outlook.com>
2024-10-27 07:26:33 +0100

    number-of-distinct-averages

--- number-of-distinct-averages.js
+++ number-of-distinct-averages.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var distinctAverages = function (nums) {
-  let a = new Set();
+  const result = new Set();
   nums.sort((a, b) => a - b);
 
   for (let i = 0; i < nums.length / 2; i++) {
-    a.add((nums[i] + nums[nums.length - i - 1]) / 2);
+    result.add((nums[i] + nums[nums.length - i - 1]) / 2);
   }
 
-  return a.size;
+  return result.size;
 };
