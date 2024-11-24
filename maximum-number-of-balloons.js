1ec194d80a7db63d30f7fd1eb5ee4ee816379da1
mlienhart <martin.lienhart@outlook.com>
2024-11-24 11:31:30 +0100

    maximum-number-of-balloons

--- maximum-number-of-balloons.js
+++ maximum-number-of-balloons.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var maxNumberOfBalloons = function (text) {
-  const a = text.split("").filter((x) => x === "b").length;
-  const b = text.split("").filter((x) => x === "a").length;
-  const c = text.split("").filter((x) => x === "l").length / 2;
-  const d = text.split("").filter((x) => x === "o").length / 2;
-  const e = text.split("").filter((x) => x === "n").length;
-
-  return Math.floor(Math.min(a, b, c, d, e));
+  return Math.floor(
+    Math.min(
+      text.split("").filter((x) => x === "b").length,
+      text.split("").filter((x) => x === "a").length,
+      text.split("").filter((x) => x === "l").length / 2,
+      text.split("").filter((x) => x === "o").length / 2,
+      text.split("").filter((x) => x === "n").length
+    )
+  );
 };
