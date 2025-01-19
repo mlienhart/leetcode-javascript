f6f6419472c82365dd91c33a1bb42d8f06c8526f
mlienhart <martin.lienhart@outlook.com>
2025-01-19 21:34:42 +0100

    maximum-difference-between-adjacent-elements-in-a-circular-array

--- maximum-difference-between-adjacent-elements-in-a-circular-array.js
+++ maximum-difference-between-adjacent-elements-in-a-circular-array.js
@@ -6,7 +6,7 @@ var maxAdjacentDistance = function (nums) {
   const circularArray = [nums[nums.length - 1], ...nums, nums[0]];
   let result = 0;
 
-  for (let i = 0; i < circularArray.length; i++) {
+  for (let i = 0; i < circularArray.length - 1; i++) {
     const absoluteDifference = Math.abs(
       circularArray[i] - circularArray[i + 1]
     );
