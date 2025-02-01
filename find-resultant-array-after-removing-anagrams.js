46af53d6e0df67a6c92e119e00e77d5fb2b75e37
mlienhart <martin.lienhart@outlook.com>
2025-02-02 00:16:20 +0100

    find-resultant-array-after-removing-anagrams

--- find-resultant-array-after-removing-anagrams.js
+++ find-resultant-array-after-removing-anagrams.js
@@ -3,21 +3,16 @@
  * @return {string[]}
  */
 var removeAnagrams = function (words) {
-  let a = [words[0]];
+  const result = [];
+  let previousWord = "";
 
-  for (let i = 0; i < words.length - 1; i++) {
-    let b = (c) => {
-      return c
-        .split("")
-        .sort((a, b) => (a > b ? 1 : -1))
-        .join("");
-    };
-    let d = b(words[i]);
-    let e = b(words[i + 1]);
-    if (d !== e) {
-      a.push(words[i + 1]);
+  for (const word of words) {
+    const sortedWord = word.split("").sort().join("");
+    if (sortedWord !== previousWord) {
+      result.push(word);
+      previousWord = sortedWord;
     }
   }
 
-  return a;
+  return result;
 };
