6dc1575f06a9c025a2d1dc7603abc09356360fda
mlienhart <martin.lienhart@outlook.com>
2025-02-16 08:49:02 +0100

    most-frequent-number-following-key-in-an-array

--- most-frequent-number-following-key-in-an-array.js
+++ most-frequent-number-following-key-in-an-array.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 var mostFrequent = function (nums, key) {
-  let a = {};
+  const counts = {};
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] === key) {
-      a[nums[i + 1]] = a[nums[i + 1]] ? a[nums[i + 1]] + 1 : 1;
+      counts[nums[i + 1]] = counts[nums[i + 1]] ? counts[nums[i + 1]] + 1 : 1;
     }
   }
 
-  return +Object.entries(a).sort((a, b) => b[1] - a[1])[0][0];
+  return +Object.entries(counts).sort((a, b) => b[1] - a[1])[0][0];
 };
