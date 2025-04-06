1dc900f88cb7a53b194eb6a0151caa78f6da4e4e
mlienhart <martin.lienhart@outlook.com>
2025-04-06 18:18:57 +0200

    maximum-containers-on-a-ship

---
+++ maximum-containers-on-a-ship.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @param {number} w
+ * @param {number} maxWeight
+ * @return {number}
+ */
+var maxContainers = function (n, w, maxWeight) {
+  return Math.min(Math.floor(maxWeight / w), n * n);
+};
