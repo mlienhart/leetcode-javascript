d0b557f1fd97d464de3db019593f6fd1573cf76b
mlienhart <martin.lienhart@outlook.com>
2025-04-27 11:15:40 +0200

    widest-vertical-area-between-two-points-containing-no-points

--- widest-vertical-area-between-two-points-containing-no-points.js
+++ widest-vertical-area-between-two-points-containing-no-points.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var maxWidthOfVerticalArea = function (points) {
-  let a = 0;
-
+  let result = 0;
   points.sort((a, b) => (a[0] > b[0] ? 1 : -1));
 
   for (let i = 1; i < points.length; i++) {
-    a = Math.max(a, points[i][0] - points[i - 1][0]);
+    result = Math.max(result, points[i][0] - points[i - 1][0]);
   }
 
-  return a;
+  return result;
 };
