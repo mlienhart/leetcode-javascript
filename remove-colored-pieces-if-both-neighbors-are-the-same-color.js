37d29c8083523e17a3a86f9bca1eb732e4a9d84a
mlienhart <martin.lienhart@outlook.com>
2025-06-22 20:55:06 +0200

    remove-colored-pieces-if-both-neighbors-are-the-same-color

--- remove-colored-pieces-if-both-neighbors-are-the-same-color.js
+++ remove-colored-pieces-if-both-neighbors-are-the-same-color.js
@@ -3,14 +3,14 @@
  * @return {boolean}
  */
 var winnerOfGame = function (colors) {
-  let a = 0;
-  let b = 0;
+  let aliceMoves = 0;
+  let bobMoves = 0;
 
   for (let i = 1; i < colors.length - 1; i++) {
     if (colors[i - 1] === colors[i] && colors[i + 1] === colors[i]) {
-      colors[i] === "A" ? a++ : b++;
+      colors[i] === "A" ? aliceMoves++ : bobMoves++;
     }
   }
 
-  return a > b;
+  return aliceMoves > bobMoves;
 };
