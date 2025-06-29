edea3d875423793af6b31f0e1c2af6755c800218
mlienhart <martin.lienhart@outlook.com>
2025-06-29 13:12:55 +0200

    maximum-subarray

--- maximum-subarray.js
+++ maximum-subarray.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let a = nums[0];
-  let b = nums[0];
+  let currentSum = nums[0];
+  let maximumSum = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    a = Math.max(nums[i], a + nums[i]);
-    b = Math.max(b, a);
+    currentSum = Math.max(nums[i], currentSum + nums[i]);
+    maximumSum = Math.max(maximumSum, currentSum);
   }
 
-  return b;
+  return maximumSum;
 };
