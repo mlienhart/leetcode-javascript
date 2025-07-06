84919ddecae45d4b988f45a9150c58680c4772d3
mlienhart <martin.lienhart@outlook.com>
2025-07-06 11:21:52 +0200

    maximum-number-of-words-you-can-type

--- maximum-number-of-words-you-can-type.js
+++ maximum-number-of-words-you-can-type.js
@@ -6,7 +6,7 @@
 var canBeTypedWords = function (text, brokenLetters) {
   text = text.split(" ");
   brokenLetters = brokenLetters.split("");
-  let result = new Set();
+  const result = new Set();
 
   for (let i = 0; i < text.length; i++) {
     for (let j = 0; j < brokenLetters.length; j++) {
