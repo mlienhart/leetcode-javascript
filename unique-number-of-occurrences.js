648cc27462ba7b045c7e3a6e61a90768321d8e75
mlienhart <martin.lienhart@outlook.com>
2025-08-02 12:01:28 +0200

    unique-number-of-occurrences

--- unique-number-of-occurrences.js
+++ unique-number-of-occurrences.js
@@ -3,20 +3,11 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function (arr) {
-  let a = [];
-  let b = new Map();
+  const map = new Map();
 
   for (let i = 0; i < arr.length; i++) {
-    b.set(arr[i], (b.get(arr[i]) || 0) + 1);
+    map.set(arr[i], (map.get(arr[i]) || 0) + 1);
   }
 
-  for (let x of b.values()) {
-    a.push(x);
-  }
-
-  if (a.length === [...new Set(a)].length) {
-    return true;
-  }
-
-  return false;
+  return map.size === new Set([...map.values()]).size;
 };
