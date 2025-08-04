c51ff6dce73e0fd33015a59b06c2575c7a9da0c3
mlienhart <martin.lienhart@outlook.com>
2025-08-04 22:39:50 +0200

    maximum-sum-of-an-hourglass

--- maximum-sum-of-an-hourglass.js
+++ maximum-sum-of-an-hourglass.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var maxSum = function (grid) {
-  let a = 0;
+  let maximumSum = 0;
 
   for (let i = 0; i < grid.length - 2; i++) {
     for (let j = 0; j < grid[0].length - 2; j++) {
-      let b =
+      const currentSum =
         grid[i][j] +
         grid[i][j + 1] +
         grid[i][j + 2] +
@@ -15,11 +15,9 @@ var maxSum = function (grid) {
         grid[i + 2][j] +
         grid[i + 2][j + 1] +
         grid[i + 2][j + 2];
-      if (b > a) {
-        a = b;
-      }
+      maximumSum = Math.max(maximumSum, currentSum);
     }
   }
 
-  return a;
+  return maximumSum;
 };
