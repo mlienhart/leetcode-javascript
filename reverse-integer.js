a46bd506439cbffa58cae170945429bf5b9e3968
mlienhart <martin.lienhart@outlook.com>
2025-08-10 13:25:24 +0200

    reverse-integer

--- reverse-integer.js
+++ reverse-integer.js
@@ -3,15 +3,7 @@
  * @return {number}
  */
 var reverse = function (x) {
-  let a = Math.abs(x).toString().split("").reverse().join("");
+  const result = Math.abs(x).toString().split("").reverse().join("");
 
-  if (a > 2 ** 31) {
-    return 0;
-  }
-
-  if (x > 0) {
-    return +a;
-  }
-
-  return -a;
+  return result > 2 ** 31 ? 0 : x > 0 ? +result : -result;
 };
