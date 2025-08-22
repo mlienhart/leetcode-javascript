46fd0233adbdb4947567523f514a32c2763784be
mlienhart <martin.lienhart@outlook.com>
2025-08-22 16:08:14 +0200

    number-of-zero-filled-subarrays

--- number-of-zero-filled-subarrays.js
+++ number-of-zero-filled-subarrays.js
@@ -3,12 +3,15 @@
  * @return {number}
  */
 var zeroFilledSubarray = function (nums) {
-  let a = 0;
-  let b = 0;
+  let totalSubarrays = 0;
+  let consecutiveZeros = 0;
 
-  for (const c of nums) {
-    c === 0 ? (b++, (a = a + b)) : (b = 0);
+  for (const number of nums) {
+    number === 0
+      ? (consecutiveZeros++,
+        (totalSubarrays = totalSubarrays + consecutiveZeros))
+      : (consecutiveZeros = 0);
   }
 
-  return a;
+  return totalSubarrays;
 };
