eb70392689060fe75d0fb77d624f8ff746ab74ba
mlienhart <martin.lienhart@outlook.com>
2025-10-19 19:28:09 +0200

    top-k-frequent-words

--- top-k-frequent-words.js
+++ top-k-frequent-words.js
@@ -4,15 +4,13 @@
  * @return {string[]}
  */
 var topKFrequent = function (words, k) {
-  let a = new Map();
+  let result = {};
 
-  for (let i = 0; i < words.length; i++) {
-    a.set(words[i], (a.get(words[i]) || 0) + 1);
+  for (const word of words) {
+    result[word] = result[word] ? result[word] + 1 : 1;
   }
 
-  let b = [...new Map(a)];
-
-  b.sort((a, b) => {
+  result = Object.entries(result).sort((a, b) => {
     if (a[1] === b[1]) {
       return a[0] > b[0] ? 1 : -1;
     }
@@ -20,5 +18,5 @@ var topKFrequent = function (words, k) {
     return a[1] < b[1] ? 1 : -1;
   });
 
-  return b.slice(0, k).map((x) => x[0]);
+  return result.slice(0, k).map((x) => x[0]);
 };
