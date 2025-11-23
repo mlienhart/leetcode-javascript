bb5d865faa57e2434f12011479937e895a7574cd
mlienhart <martin.lienhart@outlook.com>
2025-11-23 13:38:49 +0100

    concatenate-non-zero-digits-and-multiply-by-sum-i

---
+++ concatenate-non-zero-digits-and-multiply-by-sum-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var sumAndMultiply = function (n) {
+  const result = n
+    .toString()
+    .split("")
+    .map((x) => +x)
+    .filter(Boolean);
+
+  return result.join("") * result.reduce((a, b) => a + b, 0);
+};
