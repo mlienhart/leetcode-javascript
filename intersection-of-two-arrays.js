dbd47295e32d666cc258e80502b2e0cf3c515edb
mlienhart <martin.lienhart@outlook.com>
2026-04-19 13:11:25 +0200

    intersection-of-two-arrays

--- intersection-of-two-arrays.js
+++ intersection-of-two-arrays.js
@@ -4,14 +4,5 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-  const set = new Set(nums1);
-  const result = new Set();
-
-  for (let i = 0; i < nums2.length; i++) {
-    if (set.has(nums2[i])) {
-      result.add(nums2[i]);
-    }
-  }
-
-  return [...result];
+  return [...new Set(nums1).intersection(new Set(nums2))];
 };
